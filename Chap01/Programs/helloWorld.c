07bc3788e5bfbd04566eef395d685b842d4831ee
Dashaanan <0dashaanan@gmail.com>
2018-11-12 17:34:05 +0530

    Adding first 2 programs

---
+++ Chap01/Programs/helloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>				// includes information about standard library.
+
+main()							// defines a function named main that receives
+								// no argument values
+{								// statements of main are enclosed in braces.
+	printf("hello, world\n");	// main calls library funcation printf
+								// to print this sequence of characters;
+								// \n represents the newline character
+}
